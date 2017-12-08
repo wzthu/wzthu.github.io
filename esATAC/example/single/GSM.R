e2a5f8dc7bdab23c9fa40bfec1a52127f2c9c625
wzthu <wzweizheng@qq.com>
2017-12-08 08:43:57 +0800

    add report example

---
+++ esATAC/example/single/GSM.R
@@ -0,0 +1,8 @@
+library(esATAC)
+
+conclusion <- 
+  atacPipe(
+    fastqInput1 = "SRR891268_1.fastq",
+    fastqInput2 = "SRR891268_2.fastq",
+    genome = "hg19",
+    motifPWM = getMotifPWM(motif.file = system.file("extdata", "CTCF.txt", package="esATAC"), is.PWM = FALSE)))
