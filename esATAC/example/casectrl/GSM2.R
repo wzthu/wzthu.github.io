2324691f244c2af219ba962046fb3c2126656ca4
wzthu <wzweizheng@qq.com>
2017-12-09 08:10:47 +0800

    add comments

--- esATAC/example/casectrl/GSM2.R
+++ esATAC/example/casectrl/GSM2.R
@@ -1,9 +1,9 @@
 library(esATAC)
 conclusion2 <- 
     atacPipe2(
-        case=list(fastqInput1 = "SRR4435490_1.fastq",
-                  fastqInput2 = "SRR4435490_2.fastq"), 
-        control=list(fastqInput1 = "SRR4435505_1.fastq",
-                     fastqInput2 = "SRR4435505_2.fastq"),
-        genome = "mm10",
-        motifPWM = getMotifPWM(motif.file = system.file("extdata", "CTCF.txt", package="esATAC"), is.PWM = FALSE))
+        case=list(fastqInput1 = "SRR4435490_1.fastq", #case mate1 file at current working directory
+                  fastqInput2 = "SRR4435490_2.fastq"), #case mate2 file at current working directory
+        control=list(fastqInput1 = "SRR4435505_1.fastq", #control mate1 file at current working directory
+                     fastqInput2 = "SRR4435505_2.fastq"), #control mate2 file at current working directory
+        genome = "mm10", #hg19/hg38/mm9/mm10
+        motifPWM = getMotifPWM(motif.file = system.file("extdata", "CTCF.txt", package="esATAC"), is.PWM = FALSE)) #scan single motif CTCF
--- esATAC/example/single/GSM.R
+++ esATAC/example/single/GSM.R
@@ -2,7 +2,7 @@ library(esATAC)
 
 conclusion <- 
   atacPipe(
-    fastqInput1 = "SRR891268_1.fastq",
-    fastqInput2 = "SRR891268_2.fastq",
-    genome = "hg19",
-    motifPWM = getMotifPWM(motif.file = system.file("extdata", "CTCF.txt", package="esATAC"), is.PWM = FALSE))
+    fastqInput1 = "SRR891268_1.fastq", #mate1 file at current working directory
+    fastqInput2 = "SRR891268_2.fastq",#mate1 file at current working directory
+    genome = "hg19", #genome hg19/hg38/mm10/mm9
+    motifPWM = getMotifPWM(motif.file = system.file("extdata", "CTCF.txt", package="esATAC"), is.PWM = FALSE)) #scan single motif CTCF
